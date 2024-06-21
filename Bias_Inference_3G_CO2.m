f99503d54ce9014f2a7340b7e9ddf63f193a2b5e
cheerios02 <129396247+cheerios02@users.noreply.github.com>
2024-06-21 16:12:53 +0200

    Add files via upload

--- Bias_Inference_3G_CO2.m
+++ Bias_Inference_3G_CO2.m
@@ -17,30 +17,31 @@ thetas = zeros(repNum,G*Var);
 
 % Initiate the simulations
 for sim = 1:repNum
+    disp(sim)
     X = inputMCXG3_het_CO2((sim-1)*N*T+1:sim*N*T,:); % Obtain the data for X for all countries and periods in the current simulation
     Y = inputMCYG3_het_CO2((sim-1)*N*T+1:sim*N*T); % Obtain the data for Y for all countries and periods in the current simulation
     opt_group_assign = optGroup(:,sim); % Obtain the optimal group assignment for all countries in the current simulation
     which_group=zeros(N,G);
-    
+
     for g=1:G
         which_group(:,g)=(opt_group_assign==g); % Stores the optimal group of each country in the current simulation
     end
-    
+
     Y_group_aver=zeros(N*T,1);
     X_group_aver=zeros(N*T,Var);
-    
+
     country_sum_per_group=sum(which_group); % Obtain the sum of total countries in each group in the current simulation
-    
+
     for i=1:N
         if country_sum_per_group(opt_group_assign(i))>1 % If the country is not the only one in the group
             for t=1:T
-                Yt=Y(t:T:N*T); 
+                Yt=Y(t:T:N*T);
                 Y_group_aver((i-1)*T+t)=mean(Yt(opt_group_assign==opt_group_assign(i))); % For each period, obtain the average of the Y data for the countries that belong to the same group
-                Xt=X(t:T:N*T,:); 
-                X_group_aver((i-1)*T+t,:)=mean(Xt(opt_group_assign==opt_group_assign(i),:)); 
+                Xt=X(t:T:N*T,:);
+                X_group_aver((i-1)*T+t,:)=mean(Xt(opt_group_assign==opt_group_assign(i),:));
             end
         else % If the country is the only one in the group
-            for t=1:T 
+            for t=1:T
                 Yt=Y(t:T:N*T);
                 Y_group_aver((i-1)*T+t)=mean(Yt(opt_group_assign==opt_group_assign(i)));
                 Xt=X(t:T:N*T,:);
@@ -48,7 +49,7 @@ for sim = 1:repNum
             end
         end
     end
-    
+
     denominator=zeros(Var,Var, G);
     numerator=zeros(Var,G);
     theta_aux = zeros(Var,G);
@@ -62,19 +63,17 @@ for sim = 1:repNum
                 end
             end
         end
-        [m, n] = size(denominator(:,:,g));
-        if rank(denominator(:,:,g)) < min(m,n)
-            return;
-        end
         theta_aux(:,g) = denominator(:,:,g) \ numerator(:,g);
-        gitot = kron(which_group,eye(T));
         thetas(sim,:) = reshape(theta_aux,1,Var*G); % Obtain the estimates of the model parameters for each simulation
     end
 end
+
+thetas = thetas(~any(isnan(thetas),2),:);
 %% Bias:
-disp('The means of the thetas across all simulations are are:')
+true_thetas = [1.0573,1.0437,1.0311;-0.282,0.2397,0.0848;3.0497,1.6145,-0.3242;-1.5472,-1.3994,-1.2407;-0.9248,-0.9176,-0.5975;-0.0038,-0.0024,0.0001;0.0013,0.0004,-0.00006;0.0083,0.0021,0.0096];
+disp('The bias of the thetas across all simulations are are:')
 mean_theta = reshape(mean(thetas), [Var,G]);
-disp(mean_theta)
+disp(abs(1 - mean_theta./true_thetas))
 
 %% Standard Deviation
 disp('The standard deviation for theta across all simulations is:')
